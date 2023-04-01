001cd4e0585c8bfb36c25b267846a5da622192f5
yafira <yafira.martinez@lc.cuny.edu>
2023-04-01 00:23:06 -0400

    implemented header with link back to homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import Logo from '../assets/logo.png'
 import Image from 'next/image'
 import styles from '../styles/Header.module.css'
+import Link from 'next/link'
 
 export default function Header() {
 	return (
 		<div className={styles.Header}>
 			<Image className={styles.logo} src={Logo} alt='logo' />
-			<h1>cute technologies</h1>
+			<Link href='/'>
+				<h1>cute technologies</h1>
+			</Link>
+
 			<p>
 				A collection of cybertwee functional tools and resources to make your
 				computing .env cuter.
