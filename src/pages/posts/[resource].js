4b43ff17a488026ccedb1b855b27ce0c50546eb4
yafira <yafira.martinez@lc.cuny.edu>
2023-04-07 23:28:08 -0400

    fixed deployment bug

--- src/pages/posts/[resource].js
+++ src/pages/posts/[resource].js
@@ -1,17 +1,22 @@
 import ReactMarkdown from 'react-markdown'
 import { getAllPublished, getPostByResource } from '../../lib/notion.js'
 
-const Post = ({ post }) => (
-	<section>
-		<h3>{post.metadata.title}</h3>
-		<span>{post.metadata.date}</span>
-		<p>{post.metadata.tags.join(', ')}</p>
-		<ReactMarkdown>{post.markdown}</ReactMarkdown>
-	</section>
-)
+const Post = ({ post }) => {
+	if (!post) return <></>
+
+	return (
+		<section>
+			<h3>{post.metadata.title}</h3>
+			<span>{post.metadata.date}</span>
+			<p>{post.metadata.tags.join(', ')}</p>
+			<ReactMarkdown>{post.markdown}</ReactMarkdown>
+		</section>
+	)
+}
 
 export const getStaticProps = async ({ params }) => {
 	const post = await getPostByResource(params.resource)
+
 	return {
 		props: {
 			post,
@@ -22,7 +27,7 @@ export const getStaticProps = async ({ params }) => {
 
 export const getStaticPaths = async () => {
 	const posts = await getAllPublished()
-	const paths = posts.map(({ resource }) => ({ params: { resource } }))
+	const paths = posts.map(({ title }) => ({ params: { resource: title } }))
 	return {
 		paths,
 		fallback: 'blocking',
