3daf82e59b768ab1555f08cb498fb3c28e94ee96
yafira <yafira.martinez@lc.cuny.edu>
2023-04-12 00:14:12 -0400

    minor style changes to tag header

--- src/components/TagHeader.js
+++ src/components/TagHeader.js
@@ -1,11 +1,11 @@
 import styles from '../styles/TagHeader.module.css'
-import Link from 'next/link'
+// import Link from 'next/link'
 
 const TagHeader = ({ tag }) => (
 	<div className={styles.TagHeader}>
-		<Link href='/'>
-			<h1>˚✧{tag}✧˚</h1>
-		</Link>
+		{/* <Link href='/'> */}
+		<h1>✧ {tag} ✧</h1>
+		{/* </Link> */}
 	</div>
 )
 
