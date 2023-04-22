3733e13ce4afd8faad0bed4718f5f8e8321eb52f
yafira <yafira.martinez@lc.cuny.edu>
2023-04-21 23:30:41 -0400

    implemented page transition with framer motion

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
 import '@/styles/globals.css'
+import { motion } from 'framer-motion'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }) {
+	return (
+		<motion.div
+			key={router.route}
+			initial='pageInitial'
+			animate='pageAnimate'
+			variants={{
+				pageInitial: {
+					opacity: 0,
+				},
+				pageAnimate: {
+					opacity: 1,
+				},
+			}}
+		>
+			<Component {...pageProps} />
+		</motion.div>
+	)
 }
