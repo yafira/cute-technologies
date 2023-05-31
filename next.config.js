4c0e4a2af904c33078d96132de7a646fcc93f5b4
yafira <yafira.martinez@lc.cuny.edu>
2023-05-31 17:48:55 -0400

    fixed problem with cursor asset, installed mini-css

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = nextConfig
+module.exports = {
+	compiler: {
+		styledComponents: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.sanity.io',
+			},
+		],
+	},
+	webpack: (
+		config,
+		{ buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+	) => {
+		plugins: [
+			(config.module.generator.asset.publicPath = '/_next/'),
+			new MiniCssExtractPlugin({
+				experimentalUseImportModule: false,
+			}),
+		]
+		return config
+	},
+}
