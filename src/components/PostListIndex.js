92bae82a546e083b9caed87f7c1746a704d745fe
yafira <yafira.martinez@lc.cuny.edu>
2023-06-24 18:51:28 -0400

    implemented responsive masonry

--- src/components/PostListIndex.js
+++ src/components/PostListIndex.js
@@ -1,3 +1,4 @@
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 import styles from '../styles/Home.module.css'
 import PostItem from './PostItem'
 
@@ -5,17 +6,23 @@ export default function PostListTags({ posts }) {
 	if (!posts) return <h1>No resources available.</h1>
 
 	return (
-		<main className={`${styles.columns} ${styles.main}`}>
-			{posts?.map((post, index) => (
-				<PostItem
-					key={index}
-					imgURL={post.image}
-					title={post.title}
-					link={post.link}
-					description={post.description}
-					tags={post.tags}
-				/>
-			))}
+		<main className={styles.main}>
+			<ResponsiveMasonry
+				columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1000: 4, 1200: 5 }}
+			>
+				<Masonry gutter={10}>
+					{posts?.map((post, index) => (
+						<PostItem
+							key={index}
+							imgURL={post.image}
+							title={post.title}
+							link={post.link}
+							description={post.description}
+							tags={post.tags}
+						/>
+					))}
+				</Masonry>
+			</ResponsiveMasonry>
 		</main>
 	)
 }
--- src/components/PostListTags.js
+++ src/components/PostListTags.js
@@ -1,3 +1,4 @@
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 import styles from '../styles/Home.module.css'
 import PostItem from './PostItem'
 
@@ -5,18 +6,26 @@ export default function PostListIndex({ posts }) {
 	if (!posts) return <h1>No resources available.</h1>
 
 	return (
-		<main className={`${styles.columns} ${styles.main}`}>
-			{posts?.map((post, index) => (
-				<PostItem
-					key={index}
-					imgURL={post.properties.Image.url}
-					title={post.properties.Resource.title[0].text.content}
-					link={post.properties.Link.url}
-					description={post.properties.Description.rich_text[0].text.content}
-					tags={post.properties.Tags.multi_select}
-					tagPage
-				/>
-			))}
+		<main className={styles.main}>
+			<ResponsiveMasonry
+				columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1000: 4, 1200: 5 }}
+			>
+				<Masonry gutter={10}>
+					{posts?.map((post, index) => (
+						<PostItem
+							key={index}
+							imgURL={post.properties.Image.url}
+							title={post.properties.Resource.title[0].text.content}
+							link={post.properties.Link.url}
+							description={
+								post.properties.Description.rich_text[0].text.content
+							}
+							tags={post.properties.Tags.multi_select}
+							tagPage
+						/>
+					))}
+				</Masonry>
+			</ResponsiveMasonry>
 		</main>
 	)
 }
