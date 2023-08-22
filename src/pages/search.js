6ed18c6314b11917676ea517247a9bb723bff30f
yafira <yafira.martinez@lc.cuny.edu>
2023-08-22 15:49:18 -0400

    added loading feature + aligned header text to center

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { getAllPublished } from '../lib/notion'
 import SearchBar from '../components/SearchBar'
@@ -10,7 +11,20 @@ const SearchPage = ({ searchResults }) => {
 	const router = useRouter()
 	const { query } = router.query
 
-	// Filter search results based on the query
+	const [isLoading, setIsLoading] = useState(true)
+
+	useEffect(() => {
+		if (query && query.trim() !== '') {
+			// Fetch data only if query is not empty or just whitespace
+			// Simulate loading
+			setTimeout(() => {
+				setIsLoading(false) // Set loading to false after simulated loading
+			}, 2000) // Simulated delay of 2 seconds
+		} else {
+			setIsLoading(false) // No need to fetch results if query is empty
+		}
+	}, [query])
+
 	const filteredResults = searchResults.filter((result) =>
 		result.title.toLowerCase().includes(query.toLowerCase())
 	)
@@ -23,42 +37,48 @@ const SearchPage = ({ searchResults }) => {
 			<div className={styles.container}>
 				<h2>Search Results for ✧ {query} ✧</h2>
 			</div>
-			{filteredResults.length === 0 ? (
-				<p>No results found.</p>
+			{isLoading ? (
+				<p>Loading...</p>
+			) : query && query.trim() !== '' ? (
+				filteredResults.length === 0 ? (
+					<p>No results found.</p>
+				) : (
+					<div className={styles.main}>
+						<main className={styles.main}>
+							<ResponsiveMasonry
+								columnsCountBreakPoints={{
+									350: 1,
+									750: 2,
+									900: 3,
+									1000: 4,
+									1200: 5,
+								}}
+							>
+								<Masonry gutter={10}>
+									{filteredResults.map((result, index) => (
+										<PostItem
+											key={index}
+											imgURL={result.image}
+											title={result.title}
+											link={result.link}
+											description={result.description}
+											tags={result.tags}
+										/>
+									))}
+								</Masonry>
+							</ResponsiveMasonry>
+						</main>
+					</div>
+				)
 			) : (
-				<div className={styles.main}>
-					<main className={styles.main}>
-						<ResponsiveMasonry
-							columnsCountBreakPoints={{
-								350: 1,
-								750: 2,
-								900: 3,
-								1000: 4,
-								1200: 5,
-							}}
-						>
-							<Masonry gutter={10}>
-								{filteredResults.map((result, index) => (
-									<PostItem
-										key={index}
-										imgURL={result.image}
-										title={result.title}
-										link={result.link}
-										description={result.description}
-										tags={result.tags}
-									/>
-								))}
-							</Masonry>
-						</ResponsiveMasonry>
-					</main>
-				</div>
+				<p>Enter a search query to see results.</p>
 			)}
 		</div>
 	)
 }
 
-export async function getServerSideProps({ query }) {
-	const searchResults = await getAllPublished() // Fetch all published posts from the Notion API
+export async function getServerSideProps() {
+	const searchResults = await getAllPublished()
 	return { props: { searchResults } }
 }
 
